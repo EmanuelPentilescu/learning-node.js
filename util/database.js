20ce346e53f4bdc78ac5258e116355e97544ccde
EmanuelPentilescu <emanuel.pentilescu@gmail.com>
2023-05-10 11:34:50 +0300

    work on it

---
+++ util/database.js
@@ -0,0 +1,8 @@
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_ROOT, env.DATABASE_PASSWORD, {
+//     dialect: env.DATABASE_DIALECT,
+//     host: env.DATABASE_HOST
+// });
+
+// module.exports = sequelize;
